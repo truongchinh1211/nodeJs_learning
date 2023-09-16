8336cf43d60a6a353d3d5b420282054118841b27
truongchinh1211 <nntchinh2001@gmail.com>
2023-09-16 22:28:11 +0700

    update 1

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//configuration for view engine
+app.set('view engine','pug');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/trang-chu', (req, res) => {
+  res.render('pages/home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
