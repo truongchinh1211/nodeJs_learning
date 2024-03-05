d4012f7c5b8ac6cb9467c7caab020d6e31e5b54a
truongchinh1211 <nntchinh2001@gmail.com>
2024-03-06 00:00:11 +0700

    change constructure

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
+const route = require('./src/routes')
+const multer = require('multer');
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express()
-const port = 3000
 
-//routing prepare
-const route = require('./routes')
 
 //handle form-data or jason
 app.use(express.urlencoded({
   extended:true
 }));
+
 app.use(express.json());
+
 //handle static file
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,6 +29,4 @@ app.set('views', path.join(__dirname, 'resources/views'));
 //route init
 route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
